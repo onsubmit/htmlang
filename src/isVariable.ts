546ea03bf716adbcf8bba8d9c2c85a4d524e1cff
Andy <onsubmit@outlook.com>
2024-11-22 10:24:13 -0800

    Delete unused file

--- src/isVariable.ts
+++
@@ -1,5 +0,0 @@
-export function getVariableName(string: string): string | undefined {
-  const match = string.match(/^{(?<VAR_NAME>\S+)}$/);
-  const varName = match?.groups?.['VAR_NAME'];
-  return varName;
-}
